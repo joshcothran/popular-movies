3666218449dd2a529ea581166b4a4a6c1d653692
Josh Cothran <jcothran@bastille.io>
2017-09-26 19:00:40 -0400

    Configure Webpack with Hello World React app

---
+++ webpack.config.js
@@ -0,0 +1,115 @@
+const autoprefixer = require('autoprefixer');
+const fs = require('fs');
+const path = require('path');
+// const webpack = require('webpack');
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
+const paths = {
+  appBuild: resolveApp('build'),
+  appPublic: resolveApp('public'),
+  appHtml: resolveApp('public/index.html'),
+  appIndexJs: resolveApp('src/index.js'),
+  appPackageJson: resolveApp('package.json'),
+  appSrc: resolveApp('src'),
+  testsSetup: resolveApp('src/setupTests.js'),
+  appNodeModules: resolveApp('node_modules'),
+  publicPath: '/popular-movies/build/'
+};
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    paths.appIndexJs,
+  ],
+  output: {
+    path: paths.appBuild,
+    // Add /* filename */ comments to generated require()s in the output.
+    pathinfo: true,
+    filename: 'static/js/bundle.js',
+    publicPath: paths.publicPath,
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      // Process images with url-loader.
+      {
+        test: /\.(gif|jpg|jpeg|tiff|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              name: 'static/media/[name].[hash:8].[ext]',
+            }
+          }
+        ]
+      },
+      // Process JS with Babel.
+      {
+        test: /\.(js|jsx)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false,
+              presets: ['es2015', 'react', 'stage-0'],
+              compact: false,
+            }
+          }
+        ]
+      },
+      // Process css with style-loader, css-loader, and post-css-loader
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                minimize: true,
+                sourceMap: true,
+              },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: () => [
+                  require('postcss-flexbugs-fixes'),
+                  autoprefixer({
+                    browsers: [
+                      '>1%',
+                      'last 4 versions',
+                      'Firefox ESR',
+                      'not ie < 9', // React doesn't support IE8 anyway
+                    ],
+                    flexbox: 'no-2009',
+                  }),
+                ],
+              },
+            },
+          ],
+        }),
+      }
+    ]
+  },
+  plugins: [
+    // Generates an `index.html` file with the <script> injected.
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: paths.appHtml,
+    }),
+    new ExtractTextPlugin({
+      filename: 'static/css/[name].[contenthash:8].css',
+    })
+  ]
+};
