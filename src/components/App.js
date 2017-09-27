1194496f3ecd5da2bf78027abc3b0489ba5b88d2
Josh Cothran <jcothran@bastille.io>
2017-09-27 19:06:57 -0400

    Fill browser height; implement flexbox layout; update breakpoints to match tile increments

--- src/components/App.js
+++ src/components/App.js
@@ -19,21 +19,26 @@ class App extends Component {
         <div className="App-header">
           <h1 className="App-h1">POPULAR MOVIES</h1>
         </div>
-        <p>
-          isLoading: {isLoading.toString()}
-        </p>
-        <p>
-          page: {page}
-        </p>
-        <p>
-          totalPages: {totalPages}
-        </p>
-        <h2>Movies</h2>
-        <ul>
-          {movies.map((movie) => {
-            return <li key={movie.id}>{movie.title}</li>;
-          })}
-        </ul>
+        <div className="App-content">
+          <p>
+            isLoading: {isLoading.toString()}
+          </p>
+          <p>
+            page: {page}
+          </p>
+          <p>
+            totalPages: {totalPages}
+          </p>
+          <h2>Movies</h2>
+          <ul>
+            {movies.map((movie) => {
+              return <li key={movie.id}>{movie.title}</li>;
+            })}
+          </ul>
+        </div>
+        <div className="App-footer">
+            Powered by <a href="https://www.themoviedb.org/" target="_blank">The Movie DB</a>
+        </div>
       </div>
     );
   }
